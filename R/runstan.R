1add0f5babc1405dfff996ef2d958ce92704aae2
kralljr <jenna.krall@gmail.com>
2020-12-08 12:38:53 -0500

    Add endtime to output

--- R/runstan.R
+++ R/runstan.R
@@ -93,7 +93,7 @@ runstan <- function(N, typesim, stancode = NULL, stantype = NULL,
   # save relevant metadata
   sources <- dat$true$g %>% colnames()
 
-  meta <- data.frame(starttime = starttime, notes = notes, typesim = typesim, stancode = stancode,
+  meta <- data.frame(starttime = starttime, endtime = endtime, notes = notes, typesim = typesim, stancode = stancode,
                      sderr = sderr, N = N, seeds = paste(seeds, collapse = ", "),
                      iter = iter,  chains = chains, call = cl, comments = "")
   coln <- ifelse(file.exists(here("logs/sim-model-log.csv")), F, T)
